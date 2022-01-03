671bf72efc792c6e7f1b4967d195e349daf6f29f
仗键天下策码一生 <yhtyanght@163.com>
2022-01-03 19:07:35 +0800

    初始仓库记录git题解

---
+++ 9.回文数.js
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=javascript
+ *
+ * [9] 回文数
+ *
+ * https://leetcode-cn.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (58.55%)
+ * Likes:    1742
+ * Dislikes: 0
+ * Total Accepted:    855.1K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '121'
+ *
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ * 
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：x = 121
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：x = -121
+ * 输出：false
+ * 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：x = 10
+ * 输出：false
+ * 解释：从右向左读, 为 01 。因此它不是一个回文数。
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：x = -101
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * -2^31 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能不将整数转为字符串来解决这个问题吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    // 1. 转换为字符串
+    // 2. 字符串翻转
+    // 3. 字符串比较
+    let xToString = '' + x;
+    let temp = xToString;
+    let useToCompare = temp.split("").reverse().join("");
+    return xToString === useToCompare;
+};
+// @lc code=end
+
