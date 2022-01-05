aa1fe165d434857ff112588f1cc1cdc0f09843d5
仗键天下策码一生 <yhtyanght@163.com>
2022-01-05 19:56:37 +0800

    11.盛水最大的容器

---
+++ 11.盛最多水的容器.js
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ *
+ * https://leetcode-cn.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (62.21%)
+ * Likes:    3072
+ * Dislikes: 0
+ * Total Accepted:    599.3K
+ * Total Submissions: 963.4K
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为
+ * (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 
+ * 说明：你不能倾斜容器。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49 
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：height = [1,1]
+ * 输出：1
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：height = [4,3,2,1,4]
+ * 输出：16
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：height = [1,2,1]
+ * 输出：2
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * n == height.length
+ * 2 <= n <= 10^5
+ * 0 <= height[i] <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+/* var maxArea = function(height) {
+    // 穷举 循环嵌套,两次不使用同一值,不重复
+    let max = 0;
+    for(let i = 0;i < height.length -1 ; i++){
+        for(let j = i + 1; j < height.length; j++){
+            let area = (j-i) * Math.min(height[i], height[j]);
+            max = Math.max(area,max)
+        }
+    }
+    return max;
+}; */
+var maxArea = function(height) {
+    // 夹逼法
+    let max = 0;
+    for(i = 0, j = height.length - 1; i !== j;){
+        let area = (j - i) * Math.min(height[i], height[j]);
+        height[j] >= height[i] ? i++ : j--;
+        max = Math.max(area, max);
+    }
+    return max;
+};
+// @lc code=end
+
